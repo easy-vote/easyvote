fb48bb37490cf4b24a4485b8d5400b65e7ecc5a0
Howard Wells Wulsin <wulsin.1@osu.edu>
2016-11-16 20:13:24 -0800

    First commit of empty template for Single View Application

---
+++ EasyVote/EasyVote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EasyVote
+//
+//  Created by Howard Wells Wulsin on 11/16/16.
+//  Copyright © 2016 Howard Wells Wulsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
