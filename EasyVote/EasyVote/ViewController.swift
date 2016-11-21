330dbc3c382028f54b14baa7f3c80e6b2039ad2f
Howard Wells Wulsin <wulsin.1@osu.edu>
2016-11-21 09:02:49 -0800

    Add URL links

--- EasyVote/EasyVote/ViewController.swift
+++ EasyVote/EasyVote/ViewController.swift
@@ -20,6 +20,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
 
+    @IBAction func openRegister(sender: AnyObject) {
+        let url = URL(string: "https://www.vote.org/register-to-vote")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    
+    @IBAction func openCheckRegistration(sender: AnyObject) {
+        let url = URL(string: "https://www.vote.org/am-i-registered-to-vote")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+
+    @IBAction func openGetAbsentee(sender: AnyObject) {
+        let url = URL(string: "https://www.vote.org/absentee-ballot")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+
+    @IBAction func openFindPollingPlace(sender: AnyObject) {
+        let url = URL(string: "https://www.vote.org/polling-place-locator")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    
+    
 }
   
